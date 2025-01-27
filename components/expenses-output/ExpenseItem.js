c7adaa8acd54434a4af7fff8b59fd1767b0d0cb7
Debora Bucci <dbucci@protonmail.com>
2025-01-27 18:01:40 -0300

    add navigation to the ManageExpenses component, and dynamically set the title depending on from where it is navigated to

--- components/expenses-output/ExpenseItem.js
+++ components/expenses-output/ExpenseItem.js
@@ -1,10 +1,22 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
+import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({ description, date, amount }) {
+function ExpenseItem({ id, description, date, amount }) {
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+    navigation.navigate("ManageExpense", {
+      expenseId: id,
+    });
+  }
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>
@@ -60,5 +72,9 @@ const styles = StyleSheet.create({
   amount: {
     color: GlobalStyles.colors.primary500,
     fontWeight: "bold",
+  },
+
+  pressed: {
+    opacity: 0.75,
   },
 });
