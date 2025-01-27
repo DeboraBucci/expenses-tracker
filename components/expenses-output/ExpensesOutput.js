b966998569ad4457eb7f9658b2dc3e92c70115fb
Debora Bucci <dbucci@protonmail.com>
2025-01-27 14:34:38 -0300

    add styles to wrapper view in ExpensesOutput

--- components/expenses-output/ExpensesOutput.js
+++ components/expenses-output/ExpensesOutput.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
@@ -37,7 +37,7 @@ const DUMMY_EXPENSES = [
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} period={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
@@ -45,3 +45,9 @@ function ExpensesOutput({ expenses, expensesPeriod }) {
 }
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+  },
+});
