2dad987e454470b45c7f439edad30c032907c277
Debora Bucci <dbucci@protonmail.com>
2025-01-28 13:32:19 -0300

    access expenses using react context and filter them if needed

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,21 @@
+import { useContext } from "react";
 import ExpensesOutput from "../components/expenses-output/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateDaysAgo } from "../util/date";
 
 function RecentExpenses() {
-  return <ExpensesOutput expensesPeriod="Last 7 days" />;
+  const expensesCtx = useContext(ExpensesContext);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateDaysAgo(today, 7);
+
+    return expense.date > date7DaysAgo;
+  });
+
+  return (
+    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 days" />
+  );
 }
 
 export default RecentExpenses;
