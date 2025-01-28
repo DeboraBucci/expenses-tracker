02be8bd4e9a3fc027bbfd83dedd70dc037bae23d
Debora Bucci <dbucci@protonmail.com>
2025-01-28 14:24:08 -0300

    add text for the user when there are no expenses in either the recent expenses or the all expenses lists

--- components/expenses-output/ExpensesOutput.js
+++ components/expenses-output/ExpensesOutput.js
@@ -1,12 +1,15 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({ expenses, expensesPeriod }) {
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
   return (
     <View style={styles.container}>
       <ExpensesSummary expenses={expenses} period={expensesPeriod} />
-      <ExpensesList expenses={expenses} />
+      {expenses.length > 0 && <ExpensesList expenses={expenses} />}
+      {expenses.length === 0 && (
+        <Text style={styles.infoText}>{fallbackText}</Text>
+      )}
     </View>
   );
 }
@@ -16,5 +19,11 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     padding: 24,
+  },
+
+  infoText: {
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
   },
 });
